bc5c3175b02d698205b4c3066ab943eb3030ea2c
Chris Bojemski <chris.bojemski@gmail.com>
2019-01-19 15:42:01 -0500

    Open deploy branch for Heroku production build.

---
+++ build/precache-manifest.98da26541be795455cd1934a24c20269.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9b1b761c29ca77f06f1",
+    "url": "/static/js/main.a9b1b761.chunk.js"
+  },
+  {
+    "revision": "2e008bb52942ffc75113",
+    "url": "/static/js/1.2e008bb5.chunk.js"
+  },
+  {
+    "revision": "a9b1b761c29ca77f06f1",
+    "url": "/static/css/main.05755cdf.chunk.css"
+  },
+  {
+    "revision": "8cb5d04a6dc9b09dc8e60d304810c2b6",
+    "url": "/index.html"
+  }
+];
